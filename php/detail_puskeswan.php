1a29f59409335ab1428d279674c0ffdc4c538a2b
bakrianto <bakrianto@gmail.com>
2018-01-10 05:46:22 +0700

    menampilkan garis bujur dan garis lintang

--- php/detail_puskeswan.php
+++ php/detail_puskeswan.php
@@ -1,10 +1,12 @@
 <?php 
 	include 'koneksi.php';
-	$id_puskeswan = '$_GET[id_puskeswan]';
+	$id_puskeswan = $_GET['id_puskeswan'];
 	$query = "select * from puskeswan where id_puskeswan='$id_puskeswan'";
 	$q = mysqli_query($conn, $query);
 
 	while ($data = mysqli_fetch_assoc($q)) {
-		echo "<pre>",print_r($data);
+
+		echo "garis lintang :".$data[lintang];
+		echo "garis bujur :".$data[bujur];
 	}
  ?>
